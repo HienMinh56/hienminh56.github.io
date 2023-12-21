b1654e37e96fe8c4db074acf232c07b16200d6fe
Fancy <dlhminh0506@gmail.com>
2023-12-21 21:42:59 +0700

    hide Clock in mobile device

---
+++ assets/js/music-list.js
@@ -0,0 +1,39 @@
+let allMusic = [
+    {
+        name: "Harley Bird - Home",
+        artist: "Jordan Scho",
+        img: "../images/music-1",
+        src: "../audio/music-1"
+    },
+    {
+        name: "Ikson Anywhere – Ikson",
+        artist: "Audio Library",
+        img: "../images/music-2",
+        src: "../audio/music-2"
+    },
+    {
+        name: "Beauz & Jvna - Crazy",
+        artist: "Beauz & Jvna",
+        img: "../images/music-3",
+        src: "../audio/music-3"
+    },
+    {
+        name: "Hardwind - Want Me",
+        artist: "Mike Archangelo",
+        img: "../images/music-4",
+        src: "../audio/music-4"
+    },
+    {
+        name: "Jim - Sun Goes Down",
+        artist: "Jim Yosef x Roy",
+        img: "../images/music-5",
+        src: "../audio/music-5"
+    },
+    {
+        name: "Lost Sky - Vision NCS",
+        artist: "NCS Release",
+        img: "../images/music-6",
+        src: "../audio/music-6"
+    },
+
+];
